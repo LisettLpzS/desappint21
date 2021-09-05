1aa0a7a57e87e6c0250dd7ada82f1eee83ae086d
LisettLpzS <lisettlpz@gmail.com>
2021-09-04 23:28:58 -0500

    va

---
+++ p04-pagatrabajador/Program.cs
@@ -0,0 +1,31 @@
+﻿// Calcular la paga total de un trabajador
+using System;
+
+namespace p04_pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, salida;
+            int horas;
+            float pago, pagobruto, impuesto, pagoneto;
+            const float TASA = 0.3f;
+
+            Console.Write("Calculando la paga de un trabajador\n");
+            Console.Write("Dame tu nombre    ?"); nombre = Console.ReadLine();
+            Console.Write("Horas trabajadas  ?"); horas = int.Parse (Console.ReadLine());
+            Console.Write("Pago por hora     ?"); pago = float.Parse(Console.ReadLine());
+
+            pagobruto = horas * pago;
+            impuesto = pagobruto * TASA;
+            pagoneto = pagobruto - impuesto;
+
+            salida = $"\nEl trabajador {nombre}, trabajo {horas} horas, con un pago de {pago}\n"  +
+                $"Pago bruto: {pagobruto}\nImpuesto: {impuesto}\nPago neto: {pagoneto}";
+
+            Console.WriteLine(salida);
+
+        }
+    }
+}
